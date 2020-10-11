9f3b1514f3ce519c9a0517742d4b3044a8d2fecc
Folkert Saathoff <folkert@feedface.com>
2020-10-11 20:25:08 +0200

    wip

--- fliplife/mode/__init__.py
+++ fliplife/mode/__init__.py
@@ -30,42 +30,31 @@ class MODE(Enum):
     fluep     = auto()
     pipe      = auto()
 
-    @classmethod
-    def named(self,s):
-        for f in MODE:
-            if f.name == s:
-                return f
-        raise Error("unknown mode: "+s)
-
 import dotlife.clock as clock
 
-class Mode(dotMode):
 
-    def Init(mode,fluepdot,speed):
+def Class(mode):
+
+    # static import for run time checking
+    from fliplife.mode import test,read,clear,fill,gauss,echo,exec
+    from fliplife.mode import grow,pixel,dots,life,glider,guns,spawn
     
-        # static import for run time checking
-        from fliplife.mode import test,read,clear,fill,gauss,echo,exec
-        from fliplife.mode import grow,pixel,dots,life,glider,guns,spawn
-        
-        try:
-            mod = __import__("fliplife.mode."+str(mode.name), fromlist=[''])
-            cls = getattr(mod,mode.name.capitalize())
-        except AttributeError as x:
-            raise Error("class {} not found".format(mode.capitalize()))
-        except ModuleNotFoundError as x:   # ??
-            raise Error("module {} not found".format(mode.name))
-        except Exception as x:
-            raise Error("mode {} imported exception: {}".format(mode.name,str(x)))
-        
-        timer = clock.Clock.Timer(speed,repeat=True)
-        ret = cls(fluepdot,timer)
-        return ret
+    try:
+        mod = __import__("fliplife.mode."+str(mode.name), fromlist=[''])
+        ret = getattr(mod,mode.name.capitalize())
+    except AttributeError as x:
+        raise Error("class {} not found".format(mode.capitalize()))
+    except ModuleNotFoundError as x:   # ??
+        raise Error("module {} not found".format(mode.name))
+    except Exception as x:
+        raise Error("mode {} imported exception: {}".format(mode.name,str(x)))
     
+    return ret
+
+
+class Mode(dotMode):
 
     def __init__(self,fluepdot,timer):
         super().__init__(timer)
         self.fluepdot = fluepdot
 
-        
-    
-    
