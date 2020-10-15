12f809b4c57eb2c48c50622d30b04aef6a19fd0a
Folkert Saathoff <folkert@feedface.com>
2020-10-15 15:32:08 +0200

    clock mode

--- fliplife/mode/clock.py
+++ fliplife/mode/clock.py
@@ -18,27 +18,38 @@ from dotlife.effects import Morph, Morph2, Axis, Scan
 
 from enum import auto
 
-class LAYOUT(Enum):
-    large = auto()
-    small = auto()
+class Layout(Enum):
+    large  = auto()
+    small  = auto()
     dual   = auto()  
-    detail = auto()   
+    detail = auto()
+    text   = auto() 
 
+
+
+        
+        
 
 class Clock(Mode):
     
     
     
-    def run(self,msg=None,**params):
+    def run(self,stamp,**params):
 #        self.fluepdot.rendering.setMode(Fluepdot.Mode.Full)
         self.fluepdot.rendering.setMode(Fluepdot.Mode.Diff)
         
         self.small = Font(FONT.font3x5) 
         self.fixed = Font(FONT.fix3x5)
         self.large = Font(FONT.font5x5)
+        self.LARGE = Font(FONT.font5x7)
         
 
+        
         now = datetime.datetime.now()
+        if stamp != "":
+            now = datetime.datetime.fromisoformat(stamp)
+        
+        
         info("start clock: {:s}".format(now.strftime("%F %T")))
 
 
@@ -59,21 +70,16 @@ class Clock(Mode):
         
         return True
     
-    
-    
     def tick(self,now,layout,**params):
         ret = Mask()
-        now = datetime.datetime.now()
 
 
-
-        
         
         log("tick "+now.strftime("%F %T"))
-        if layout in [ LAYOUT.small, LAYOUT.large]:
+        if layout in [ Layout.small, Layout.large]:
             date_time = now.strftime("%F %T")
             font = self.large
-            if layout == LAYOUT.small:
+            if layout == Layout.small:
                 font = self.fixed
             w = FRAMESIZE.w - len(date_time)*(font.size.w+1)
             pos0 = Position( int(ceil(w/2)), 6 )
@@ -83,7 +89,7 @@ class Clock(Mode):
 
 
     
-        elif layout == LAYOUT.dual:
+        elif layout == Layout.dual:
             date = now.strftime("%F")
             time = now.strftime("%T")
             w0 = FRAMESIZE.w - len(date)*(self.large.size.w+1)
@@ -95,69 +101,58 @@ class Clock(Mode):
             text = self.large.render(time,fixed=True)
             ret.addMask(text,pos=pos1)
     
-        else:
+    
+        elif layout == Layout.text:
+            pos0 = Position(0,1)
+            pos1 = Position(0,10)
+            txt = humanreadable( now ).split("\n")
+                
+            txt0 = txt[0]
+            txt1 = txt[1]
+            text0 = self.small.render(txt0.upper(),fixed=False)
+            text1 = self.small.render(txt1.upper(),fixed=False)
+            ret.addMask(text0,pos=pos0)
+            ret.addMask(text1,pos=pos1)
+            
+            pass
+
+    
+        elif layout == Layout.detail:
             date_time = now.strftime("%F %T")
             pos0 = Position(0,1)
+            pos1 = Position(0,10)
             
+            start_decade = now.replace(year=(now.year-now.year%10),month=1,day=1,hour=0,minute=0,second=0,microsecond=0)
+            start_year   = now.replace(                            month=1,day=1,hour=0,minute=0,second=0,microsecond=0)
+            start_day    = now.replace(                                          hour=0,minute=0,second=0,microsecond=0)
             
-            year_start = now.replace(month=1,day=1,hour=0,minute=0,second=0,microsecond=0)
-            day_start = now.replace(hour=0,minute=0,second=0,microsecond=0)
-            decade_start = now.replace(year=2020,month=1,day=1,hour=0,minute=0,second=0,microsecond=0)
+            seconds_decade = (start_decade.replace(year=start_decade.year+10) - start_decade).total_seconds()
+            seconds_year   = (start_year.replace(year=start_year.year+1) - start_year).total_seconds()
+            seconds_day    = datetime.timedelta(days=1).total_seconds()             
             
-            year_delta = now - year_start
-            day_delta = now - day_start
-            decade_delta = now - decade_start
-            
+            fraction_decade = (now - start_decade).total_seconds() / seconds_decade * 100.
+            fraction_year   = (now - start_year).total_seconds() / seconds_year * 100.
+            fraction_day    = (now - start_day).total_seconds() / seconds_day * 100.
 
-            seconds_per_day = 24. * 60. * 60.
-            seconds_per_year = 365. * seconds_per_day
-            seconds_per_decade = 10. * seconds_per_year
- 
-            
-            daydone = day_delta.total_seconds() / seconds_per_day * 100.
-            yeardone = year_delta.total_seconds() / seconds_per_year * 100.
-            decdone = decade_delta.total_seconds() / seconds_per_decade * 100.
-            
-#            detail = "20s:{:4.1f}% YEAR:{:5.1f}% DAY:{:5.1f}% ".format(decdone,yeardone,daydone)
-            detail = "YEAR:{:5.1f}% COMPLETE DAY:{:5.1f}%".format(yeardone,daydone)
-#            detail = "YEAR {:5.1f}% OVER".format(yeardone)           
-            
-            
-#            day = "DAY{:3.0f}% DONE.".format(seconds_today / seconds_per_day)
-#           detail = detail.replace("DAY",now.strftime("%a").upper())
-#            year = "YEAR{:3.0f}% DONE.".format(50)
-#            detail = detail.replace("YEAR",now.strftime("%Y"))
-#            decade = "DEC:{:5.1f}%".format(0.5)
-#            decade = decade.replace("DEC",now.strftime("%Y")[0:2]+"s")
-
-            pos1 = Position( 0, 8)
-#            pos1 = Position( 2+(9  +4  + 3  ) * 4, 8 )
-#            pos2 = Position( ( 0   ) * 4, 8 )
-#            pos3 = Position( 4+(9+10)* 4, 8 )
+            detail = "DECADE:{:3.0f}% YEAR:{:3.0f}% DAY:{:3.0f}%".format(fraction_decade,fraction_year,fraction_day)
 
             text = self.large.render(date_time,fixed=True)
             ret.addMask(text,pos=pos0)
-
             text = self.small.render(detail,fixed=False,space=3)
             ret.addMask(text,pos=pos1)
 
-#            text = self.small.render(day,fixed=False,space=3)
-#            ret.addMask(text,pos=pos1)
-#            text = self.small.render(year,fixed=False,space=3)
-#           ret.addMask(text,pos=pos2)
-##            text = self.small.render(decade,fixed=False,space=3)
-##            ret.addMask(text,pos=pos3)
-    
     
         return ret
     
 
-    def draw(self,**params):
+    def draw(self,stamp,**params):
 
 
         ret = []
 
         now = datetime.datetime.now()
+        if stamp != "":
+            now = datetime.datetime.fromisoformat(stamp)
         
         next = self.tick(now,**params)
 
@@ -176,7 +171,95 @@ class Clock(Mode):
 
         
     flags = [
-        ("l:", "layout=", "layout", LAYOUT.large, "clock layout", lambda x: LAYOUT[x] ),
+        ("l:", "layout=", "layout", Layout.large, "clock layout", lambda x: Layout[x] ),
+        ("", "stamp=", "stamp", "", "timestamp", None ),
     ]
     
     
+
+
+
+def humanreadable(then):
+        ret = ""
+        year,month,day,hour,minute,_,weekday,_,_ = then.timetuple()
+
+        ret = "Ein "
+        ret += {
+            0: "Montag",
+            1: "Dienstag",
+            2: "Mittwoch",
+            3: "Donnerstag",
+            4: "Freitag",
+            5: "Samstag",
+            6: "Sonntag",
+        }[weekday%7]
+
+        ret += " im "
+        ret += {
+             1: "Januar",
+             2: "Februar",
+             3: "März",
+             4: "April",
+             5: "Mai",
+             6: "Juni",
+             7: "Juli",
+             8: "August",
+             9: "September",
+            10: "Oktober",
+            11: "November",
+             0: "Dezember",
+        }[month%12]
+        ret += ", \n"
+
+        x = 0
+        if minute < 10:
+            ret += "kurz nach"
+        elif minute < 20:
+            ret += "viertel nach"
+        elif minute < 29:
+            ret += "kurz vor halb"
+            x = 1
+        elif minute < 32:
+            ret += "halb"
+            x = 1
+        elif minute < 40:
+            ret += "kurz nach halb"
+            x = 1
+        elif minute < 50:
+            ret += "viertel vor"
+            x = 1
+        else:
+            ret += "kurz vor"
+            x = 1
+
+        ret += " "
+        ret += {
+            1: "Eins",
+            2: "Zwei",
+            3: "Drei",
+            4: "Vier",
+            5: "Fünf",
+            6: "Sechs",
+            7: "Sieben",
+            8: "Acht",
+            9: "Neun",
+           10: "Zehn",
+           11: "Elf",
+            0: "Zwölf",
+        }[(hour+x)%12]
+        if minute < 10 or minute >= 50:
+            ret += " Uhr"
+
+        if hour+x >= 22:
+            ret += " Nachts"
+        elif hour+x >= 19:
+            ret += " Abends"
+        elif hour+x >= 11:
+            ret += ""
+        elif hour+x >= 6:
+            ret += " Morgens"
+        else:
+            ret += " Nachts"
+
+        ret += "..."
+        return ret
